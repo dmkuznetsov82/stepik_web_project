de8d7e54eea87545a557839aaf4b617bb22de996
Aleksey Frolov <atommixz@gmail.com>
2017-06-22 00:45:43 +0300

    add upstream server: fix

--- hello.py
+++ hello.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python2
 # coding: utf-8
-#from cgi import parse_qs, escape
-from urlparse import parse_qs
 
 def wsgi_application(environ, start_response):
-	# бизнес-логика
 	status = '200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
 	]
 
-	#params = parse_qs(environ['QUERY_STRING'])
-	
-	#body = ''
-	#for k in sorted(params.iterkeys()):
-	#	body += k + "=".join(params[k]) + "\r\n"
-
-	body = [bytes(i + '\r\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+	body = environ['QUERY_STRING'].replace('&', '\r\n')
 
 	start_response(status, headers)
-	return body 
+	return [body]
