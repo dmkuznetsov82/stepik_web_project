47f6dcf57101caba17b6f8516694b89f609e940f
Aleksey Frolov <atommixz@gmail.com>
2017-06-22 23:24:42 +0300

    init fix

--- init.sh
+++ init.sh
@@ -3,9 +3,12 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/hello.gunicorn.conf   /etc/gunicorn.d/hello.py
-sudo ln -s /home/box/web/etc/ask.gunicorn.conf   /etc/gunicorn.d/ask.py
-sudo /etc/init.d/gunicorn restart
-#gunicorn -с /etc/gunicorn.d/hello.py hello
+#sudo ln -s /home/box/web/etc/hello.gunicorn.conf   /etc/gunicorn.d/hello.py
+#sudo ln -s /home/box/web/etc/ask.gunicorn.conf   /etc/gunicorn.d/ask.py
+#sudo /etc/init.d/gunicorn restart
+gunicorn -c etc/hello.gunicorn.conf hello:wsgi_application
+
+cd ask
+gunicorn -c ../etc/ask_local.gunicorn.conf ask.wsgi:application
 
 sudo /etc/init.d/mysql start
