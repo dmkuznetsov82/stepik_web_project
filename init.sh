a8ff5aa641579263948dcbbc7056ed2f48d0ff1f
Aleksey Frolov <atommixz@gmail.com>
2017-06-22 23:11:33 +0300

    add ask

--- init.sh
+++ init.sh
@@ -3,7 +3,8 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.gunicorn.conf   /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/ask.gunicorn.conf   /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
 #gunicorn -с /etc/gunicorn.d/hello.py hello
 
