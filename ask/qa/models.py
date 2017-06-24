b5c8a18acd3ece9af247263920a5dd120d9f199f
Aleksey Frolov <atommixz@gmail.com>
2017-06-24 07:46:27 +0300

    upd

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-# Question - вопрос
-# title - заголовок вопроса
-# text - полный текст вопроса
-# added_at - дата добавления вопроса
-# rating - рейтинг вопроса (число)
-# author - автор вопроса
-# likes - список пользователей, поставивших "лайк"
+class QuestionManager(models.Manager):
+    def new():
+        pass
+
+    def popular():
+        pass
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField()
@@ -19,24 +19,10 @@ class Question(models.Model):
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
 
-class QuestionManager(models.Manager):
-    def new():
-        pass
 
-    def popular():
-        pass
-
-# Answer - ответ
-# text - текст ответа
-# added_at - дата добавления ответа
-# question - вопрос, к которому относится ответ
-# author - автор ответа
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField()
     question = models.ForeignKey(Question)
     author = User
 
-# QuestionManager - менеджер модели Question
-# new - метод возвращающий последние добавленные вопросы
-# popular - метод возвращающий вопросы отсортированные по рейтингу
